a8f1cc36333aee993d24cd3e01ad063f138febbb
Katie <katiefaybehrmann@gmail.com>
2022-10-31 20:44:09 -0600

    commit tree 030e94a0bb85ad271a12380e8ecc0d4c2f4109b0

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let nummy = 100;
+let random = Math.floor(Math.random() * nummy);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 2, 6, 20);
+
